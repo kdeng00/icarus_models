8d190f4c907f3fc41c6196a4cb81dc0a51d39864
KD <kundeng94@gmail.com>
2025-03-01 21:33:06 +0000

    Finished user model

--- models/user.go
+++ models/user.go
@@ -1,5 +1,20 @@
 package models
 
+import (
+	"fmt"
+	"time"
+)
+
 type User struct {
-	Username string
+	Id            int    `json:"id"`
+	Username      string `json:"username"`
+	Password      string `json:"password"`
+	Email         string `json:"email"`
+	Phone         string `json:"phone"`
+	Firstname     string `json:"firstname"`
+	Lastname      string `json:"lastname"`
+	EmailVerified bool   `json:"email_verified"`
+	DateCreated   time   `json:"date_created"`
+	Status        string `json:"status"`
+	LastLogin     time   `json:"last_login"`
 }
