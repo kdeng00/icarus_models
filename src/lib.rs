ae2fd072293eab347ce04163c5ff34c325b19748
phoenix <kundeng94@gmail.com>
2025-03-22 20:31:03 +0000

    Removing test (#14)
    Reviewed-on: https://git.kundeng.us/phoenix/icarus-models/pulls/14
    Co-authored-by: phoenix <kundeng94@gmail.com>
    Co-committed-by: phoenix <kundeng94@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,3 @@ pub mod song;
 pub mod token;
 pub mod types;
 pub mod user;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
